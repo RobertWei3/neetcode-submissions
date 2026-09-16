bfc17823c122bd69bd0fb6b85d12032001f2e0d6
YUFAN WEI <110293175+RobertWei3@users.noreply.github.com>
2026-09-16 17:07:36 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,38 @@
+class TreeNode:
+
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+ 
+    def insert(self, word: str) -> None:
+        curr = self.root 
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TreeNode()
+            curr = curr.children[c]
+        curr.is_word = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.is_word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+        
+        
