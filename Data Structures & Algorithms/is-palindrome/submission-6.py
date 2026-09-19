94c77149ef8cd6c4d8a21844807d41f566257a93
YUFAN WEI <110293175+RobertWei3@users.noreply.github.com>
2026-09-19 14:26:02 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # s = "".join(char for char in s if char.isalnum())
+        # 不要这么创建s，空间复杂度会变为n
+        i, j = 0, len(s) -1
+
+        while i < j:
+            # 遇到不是数字的就跳过
+            while i < j and not s[i].isalnum():
+                i += 1
+
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+            
+        return True
+
