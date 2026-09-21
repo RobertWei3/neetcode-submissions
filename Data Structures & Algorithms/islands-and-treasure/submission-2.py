5819c29d96ca1817dc9b0459f0e2a18701cd8beb
YUFAN WEI <110293175+RobertWei3@users.noreply.github.com>
2026-09-21 14:28:52 -0400

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,130 @@
+# class Solution:
+#     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+#         # if not grid:
+#         #     return []
+
+
+#         ROWS, COLS = len(grid), len(grid[0])
+#         # visit = set()
+#         directions = [[1,0], [-1,0], [0,1], [0,-1]]
+#         INF = 2147483647
+
+#         def bfs(r, c):
+#             q = deque([(r,c)])
+#             visit = [[False] * COLS for _ in range(ROWS)]
+#             visit[r][c] = True
+#             # q.append((r,c))
+#             steps = 0
+
+#             while q:
+#                 for _ in range(len(q)):
+#                     row, col = q.popleft()
+#                     if grid[row][col] == 0:
+#                         return steps
+#                     for dr, dc in directions:
+#                         nr, nc = row+dr, col+dc
+#                         # if ((row,col) in visit or min(col, row) < 0 or rol == ROWS or col == COLS or grid[row][col] == -1):
+#                         #     continue
+#                         if (0 <= nr < ROWS and 0 <=nc < COLS and not visit[nr][nc] and grid[nr][nc] != -1):
+#                             visit[nr][nc] = True
+#                         # if grid[row][col] == 0:
+#                         #     count += 1
+#                             q.append((nr, nc))
+#                         # visit.add(row, col)
+#                 steps += 1
+#             return INF
+
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if grid[r][c] == INF:
+#                     grid[r][c] = bfs(r,c)
+
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == 0:
+                    q.append((row, col))
+                    visit.add((row, col))
+
+
+        def addRoom(r, c):
+            if (r < 0 or r == ROWS or c<0 or c == COLS or (r,c) in visit or grid[r][c] == -1):
+                return
+            q.append((r,c))
+            visit.add((r,c))
+
+
+        dist = 0
+        while q:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addRoom(r+1, c)
+                addRoom(r-1, c)
+                addRoom(r, c+1)
+                addRoom(r, c-1)
+            dist+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # ROWS, COLS = len(grid), len(grid[0])
+        # directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        # q = deque()
+        # visit = [[False] * COLS for _ in range(ROWS)]
+
+        # # 所有宝箱一起作为起点
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if grid[r][c] == 0:
+        #             q.append((r, c))
+        #             visit[r][c] = True
+
+        # steps = 0
+        # while q:
+        #     for _ in range(len(q)):
+        #         row, col = q.popleft()
+        #         grid[row][col] = steps          # 出队时写入距离
+        #         for dr, dc in directions:
+        #             nr, nc = row + dr, col + dc
+        #             if (0 <= nr < ROWS and 0 <= nc < COLS
+        #                     and not visit[nr][nc] and grid[nr][nc] != -1):
+        #                 visit[nr][nc] = True
+        #                 q.append((nr, nc))
+        #     steps += 1
+
+
+
+
+
+
+
